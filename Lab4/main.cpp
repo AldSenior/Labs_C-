eb4485a57738477b9be5ffb70ae8f71851ba58d6
AldSenior <aldsio196@gmail.com>
2025-03-28 15:40:45 +0300

    +

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,82 +1,278 @@
-#include <cstring> // Для работы с C-строками (strlen, strstr, strcpy и т.д.)
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-// Функция для проверки, содержит ли слово все символы подстроки
+// ================== ОБЩИЕ ФУНКЦИИ ==================
+
+// Проверка наличия всех символов подстроки в слове (без учета порядка)
 bool containsAllChars(const char *word, const char *substr) {
-  for (int i = 0; substr[i] != '\0'; i++) {
-    if (strchr(word, substr[i]) == nullptr) {
-      return false; // Если символ подстроки не найден в слове
-    }
+  if (!substr || !*substr)
+    return true;
+  if (!word)
+    return false;
+
+  // Создаем копии, чтобы не изменять исходные указатели
+  string wordStr(word);
+  string substrStr(substr);
+
+  // Для каждого символа в подстроке проверяем его наличие в слове
+  for (char c : substrStr) {
+    auto pos = wordStr.find(c);
+    if (pos == string::npos)
+      return false; // символ не найден
+    wordStr.erase(pos, 1);
   }
-  return true; // Все символы подстроки найдены в слове
+  return true;
 }
 
-// Функция для формирования новой строки из слов, содержащих все символы
-// подстроки
-void formNewString(const char *input, const char *substr, char *result) {
-  char temp[1000];     // Временный буфер для копирования строки
-  strcpy(temp, input); // Копируем входную строку в temp
-  result[0] = '\0';    // Инициализируем результирующую строку пустой
+// Проверка наличия подстроки в слове (с учетом регистра и порядка)
+bool containsSubstring(const char *word, const char *substr) {
+  if (!substr || !*substr)
+    return true;
+  return strstr(word, substr) != nullptr;
+}
 
-  char *word = strtok(temp, " "); // Разбиваем строку на слова
-  while (word != nullptr) {
+// ================== ЗАДАЧА 1 ==================
+
+void createFilteredString(char *&result, const char *input,
+                          const char *substr) {
+  size_t inputLen = strlen(input);
+  result = new char[inputLen + 1];
+  result[0] = '\0';
+  char *buffer = new char[inputLen + 1];
+  strcpy(buffer, input);
+
+  char *word = strtok(buffer, " ");
+  while (word) {
     if (containsAllChars(word, substr)) {
-      strcat(result, word); // Добавляем слово в результат
-      strcat(result, " ");  // Добавляем пробел после слова
-    }
-    word = strtok(nullptr, " "); // Переходим к следующему слову
-  }
-}
-
-// Функция для перемещения первого слова, содержащего подстроку, в начало строки
-void moveFirstWordToStart(char *input, const char *substr) {
-  char temp[1000];     // Временный буфер для копирования строки
-  strcpy(temp, input); // Копируем входную строку в temp
-
-  char *firstWord = nullptr;    // Указатель на первое найденное слово
-  char *restOfString = nullptr; // Указатель на оставшуюся часть строки
-
-  char *word = strtok(temp, " "); // Разбиваем строку на слова
-  while (word != nullptr) {
-    if (strstr(word, substr) != nullptr) { // Если слово содержит подстроку
-      firstWord = word;
-      restOfString = strtok(nullptr, ""); // Оставшаяся часть строки
-      break;
+      strcat(result, word);
+      strcat(result, " ");
     }
     word = strtok(nullptr, " ");
   }
 
-  if (firstWord != nullptr) {
-    char newString[1000];
-    strcpy(newString, firstWord);    // Начинаем новую строку с найденного слова
-    strcat(newString, " ");          // Добавляем пробел
-    strcat(newString, restOfString); // Добавляем оставшуюся часть строки
-    strcpy(input, newString);        // Обновляем исходную строку
+  if (result[0]) {
+    result[strlen(result) - 1] = '\0';
   }
+
+  delete[] buffer;
 }
 
-int main() {
-  char input[1000];
-  char substr[100];
-  char result[1000] = "";
+void moveFirstSubstringWord(char *&input, const char *substr) {
+  char **words = nullptr;
+  size_t wordCount = 0;
+  char *firstMatch = nullptr;
+  char *context = nullptr;
 
+  size_t inputLen = strlen(input);
+  char *tempInput = new char[inputLen + 1];
+  strcpy(tempInput, input);
+
+  // Первый проход: подсчет количества слов
+  char *word = strtok_r(tempInput, " ", &context);
+  while (word) {
+    if (!firstMatch && containsSubstring(word, substr)) {
+      firstMatch = new char[strlen(word) + 1];
+      strcpy(firstMatch, word);
+    } else {
+      wordCount++;
+    }
+    word = strtok_r(nullptr, " ", &context);
+  }
+
+  // Выделение памяти для слов
+  if (wordCount > 0) {
+    words = new char *[wordCount];
+  }
+
+  // Второй проход: сохранение слов
+  strcpy(tempInput, input);
+  context = nullptr;
+  word = strtok_r(tempInput, " ", &context);
+  size_t index = 0;
+  while (word) {
+    if (firstMatch && strcmp(word, firstMatch) == 0) {
+      // Пропускаем первое совпадающее слово
+      word = strtok_r(nullptr, " ", &context);
+      continue;
+    }
+    if (index < wordCount) {
+      words[index] = new char[strlen(word) + 1];
+      strcpy(words[index], word);
+      index++;
+    }
+    word = strtok_r(nullptr, " ", &context);
+  }
+
+  // Сборка результата
+  size_t newLen = (firstMatch ? strlen(firstMatch) : 0);
+  for (size_t i = 0; i < wordCount; i++) {
+    newLen += strlen(words[i]) + 1;
+  }
+
+  char *newInput = new char[newLen + 1];
+  newInput[0] = '\0';
+
+  if (firstMatch) {
+    strcat(newInput, firstMatch);
+    for (size_t i = 0; i < wordCount; i++) {
+      strcat(newInput, " ");
+      strcat(newInput, words[i]);
+    }
+  }
+
+  // Освобождение памяти и замена указателя
+  delete[] input;
+  input = newInput;
+
+  delete[] tempInput;
+  if (firstMatch) {
+    delete[] firstMatch;
+  }
+  for (size_t i = 0; i < wordCount; i++) {
+    delete[] words[i];
+  }
+  delete[] words;
+}
+
+void task1() {
+  int inp_size = 1000;
+  int substr_size = 100;
+  char *input = new char[inp_size];
+  char *substr = new char[substr_size];
+  char *result = nullptr;
+
+  cout << "=== Обработка Char-строк ===" << endl;
+  cout << "Входная строка: ";
+  cin.getline(input, inp_size);
+
+  cout << "Целевая подстрока: ";
+  cin.getline(substr, substr_size);
+
+  // Фильтрация по наличию всех символов
+  createFilteredString(result, input, substr);
+  cout << "\nРезультат фильтрации: "
+       << (result && result[0] ? result : "<пусто>") << endl;
+
+  // Перемещение первого слова с подстрокой
+  char *modified = new char[strlen(input) + 1];
+  strcpy(modified, input);
+  moveFirstSubstringWord(modified, substr);
+
+  if (strcmp(modified, input) != 0) {
+    cout << "После перемещения: " << modified << endl;
+  } else {
+    cout << "Подходящие слова не найдены" << endl;
+  }
+
+  // Освобождение памяти
+  delete[] input;
+  delete[] substr;
+  if (result)
+    delete[] result;
+  delete[] modified;
+}
+
+// ================== ЗАДАЧА 2 ==================
+
+string *splitString(const char *str, size_t *wordCount) {
+  *wordCount = 0;
+  if (!str || !*str)
+    return nullptr;
+
+  // Первый проход: подсчет количества слов
+  size_t count = 0;
+  bool inWord = false;
+  for (const char *p = str; *p; ++p) {
+    if (*p != ' ') {
+      if (!inWord) {
+        ++count;
+        inWord = true;
+      }
+    } else {
+      inWord = false;
+    }
+  }
+
+  if (count == 0)
+    return nullptr;
+
+  string *words = new string[count];
+
+  // Второй проход: заполнение массива словами
+  size_t index = 0;
+  const char *wordStart = nullptr;
+  inWord = false;
+
+  for (const char *p = str;; ++p) {
+    if (*p && *p != ' ') {
+      if (!inWord) {
+        wordStart = p;
+        inWord = true;
+      }
+    } else {
+      if (inWord) {
+        words[index++] = string(wordStart, p);
+        inWord = false;
+      }
+      if (!*p)
+        break;
+    }
+  }
+
+  *wordCount = count;
+  return words;
+}
+void task2() {
+  cout << "\n=== Разбиение строки ===" << endl;
+
+  string input;
   cout << "Введите строку: ";
-  cin.getline(input, 1000); // Считываем строку с пробелами
+  getline(cin, input);
 
-  cout << "Введите подстроку: ";
-  cin.getline(substr, 100); // Считываем подстроку
+  size_t wordCount = 0;
+  string *words = splitString(input.c_str(), &wordCount);
 
-  // Задание 1
-  formNewString(input, substr, result);
-  cout << "Новая строка из слов, содержащих все символы подстроки: " << result
-       << endl;
+  if (words) {
+    cout << "Полученные слова (" << wordCount << "):" << endl;
+    for (size_t i = 0; i < wordCount; i++) {
+      cout << "[" << i + 1 << "] " << words[i] << endl;
+    }
 
-  // Задание 2
-  moveFirstWordToStart(input, substr);
-  cout << "Строка после перемещения первого слова с подстрокой в начало: "
-       << input << endl;
+    delete[] words;
+  } else {
+    cout << "Строка пуста!" << endl;
+  }
+}
+// ================== ГЛАВНОЕ МЕНЮ ==================
+int main() {
+  int choice;
+  do {
+    cout << "\nМеню:\n"
+         << "1. Обработка Char-строк\n"
+         << "2. Разбиение строки\n"
+         << "0. Выход\n"
+         << "Выбор: ";
+    cin >> choice;
+    cin.ignore();
+
+    switch (choice) {
+    case 1:
+      task1();
+      break;
+    case 2:
+      task2();
+      break;
+    case 0:
+      cout << "Завершение работы." << endl;
+      break;
+    default:
+      cout << "Неверный выбор!" << endl;
+    }
+  } while (choice != 0);
 
   return 0;
 }
